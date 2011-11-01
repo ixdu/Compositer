073b1adae5162a2054cead406c1c2aa8d1517c61
Freeze <freeze@2du.ru>
2011-11-01 11:59:34 +0600

    version change 0.6.3 -> 0.6.4

--- Compositer.js
+++ Compositer.js
@@ -15,7 +15,7 @@
   You should have received a copy of the GNU Affero General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.'
 
-  version 0.6.3
+  version 0.6.4
 */
 
 /* Compositer */
